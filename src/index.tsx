3da20dfe0f05c23fd423dbc4a12dcf5735c52339
Andrew Huth <andrew@huth.me>
2021-05-08 23:44:39 -0700

    Group operations together in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import Conway from './conway';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const game = Conway(100);
   game.randomize();
-  game.draw(canvas);
   game.debug();
+
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  game.draw(canvas);
+
+  const nextButton = document.getElementById('next') as HTMLButtonElement;
+  nextButton.addEventListener('click', function () {
+    game.tick();
+    game.draw(canvas);
+  });
 });
