e09105b636d97b9f9095050aa46b667280cdc659
Andrew Huth <andrew@huth.me>
2021-05-09 09:04:48 -0700

    Remove now-unnecessary debugging info

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import './index.css';
 document.addEventListener('DOMContentLoaded', function () {
   const game = Conway(100);
   game.randomize();
-  game.debug();
 
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   game.draw(canvas);
