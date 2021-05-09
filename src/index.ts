6750138d2adc829a924e1c5850eed4c6cc2618b7
Andrew Huth <andrew@huth.me>
2021-05-09 09:14:44 -0700

    Run the game in an animation frame

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,34 @@ import Conway from './conway';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', function () {
+  let playing = false;
+
   const game = Conway(100);
   game.randomize();
 
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   game.draw(canvas);
 
-  const nextButton = document.getElementById('next') as HTMLButtonElement;
-  nextButton.addEventListener('click', function () {
-    game.next();
-    game.draw(canvas);
+  const toggleButton = document.getElementById('toggle') as HTMLButtonElement;
+  toggleButton.addEventListener('click', function () {
+    // The toggle button was clicked, so determine if we should start or stop the game from running.
+    if (playing) {
+      // The game was running, so stop everything.
+      playing = false;
+      toggleButton.textContent = 'Start';
+    } else {
+      // The game wasn't running, so start it up.
+      playing = true;
+      toggleButton.textContent = 'Stop';
+
+      function compute() {
+        if (!playing) { return; }
+        game.next();
+        game.draw(canvas);
+        window.requestAnimationFrame(compute);
+      }
+
+      window.requestAnimationFrame(compute);
+    }
   });
 });
