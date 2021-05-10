35c52b27909b0b38b56b925d0584dc32ff7f0174
Andrew Huth <andrew@huth.me>
2021-05-10 09:31:02 -0700

    Re-organize index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import './index.css';
 
 document.addEventListener('DOMContentLoaded', function () {
   let playing = false;
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  const toggleButton = document.getElementById('toggle') as HTMLButtonElement;
 
   const game = Conway(250);
   game.randomize();
-
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   game.draw(canvas);
 
-  const toggleButton = document.getElementById('toggle') as HTMLButtonElement;
   toggleButton.addEventListener('click', function () {
     // The toggle button was clicked, so determine if we should start or stop the game from running.
     if (playing) {
